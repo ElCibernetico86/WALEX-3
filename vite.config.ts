9283bff952fec6019a648ebc52d9ace1724c4efb
ElCibernetico86 <elcibernetico86@gmail.com>
2026-04-21 14:10:18 -0500

    Deploy 100M Offers Funnel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/walex-preview/',
+    base: '/Walex-3/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
